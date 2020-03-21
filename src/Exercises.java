d18ada4f3dc14ddb0d5c6ff7d45191ff59af29dc
terzoc <doctort123@gmail.com>
2020-03-21 12:21:17 -0400

    fixed exericse 1 again

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Exercises {
 
   public int findMe(int[] list, int target) {
+    if (list == null) {
+      return -1;
+    }
     for (int i = 0; i < list.length; i++) {
           if (list[i] == target)
               return i;
