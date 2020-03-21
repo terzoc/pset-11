5ab1d8a93a3ff203915dff42b1dd203e696e3475
terzoc <doctort123@gmail.com>
2020-03-21 12:51:02 -0400

    exercise 3 done

--- src/Exercises.java
+++ src/Exercises.java
@@ -26,7 +26,24 @@ public class Exercises {
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
-    return -1;
+    if (list == null) {
+     return -1;
+   }
+   int start = 0;
+   int middle = 0;
+   int end = list.size() - 1;
+
+   while (start <= end) {
+     middle = (start + end) / 2;
+     if (list.get(middle) < target) {
+       start = middle + 1;
+     }else if (list.get(middle) > target) {
+       end = middle - 1;
+     }else if (list.get(middle) == target) {
+       return middle;
+     }
+   }
+   return -1;
   }
 
   public int findMeFaster(String[] list, String target) {
