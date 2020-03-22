35b450eab9dc6c70a6e73b71cc6cc9eb39ae0d9a
terzoc <doctort123@gmail.com>
2020-03-22 19:46:39 -0400

    start 7

--- src/Exercises.java
+++ src/Exercises.java
@@ -108,7 +108,22 @@ public class Exercises {
   }
 
   public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
-    return null;
+    if (list == null || list.size() == 0) {
+          return null;
+      }
+
+      if (ascending) {
+          for (int i = 0; i < list.size() -1; i++) {
+              for (int j = 0; j < list.size()-1-i; j++) {
+                  if (list.get(j).compareTo(list.get(j+1)) > 0) {
+                      String temp = list.get(j);
+                      list.set(j, list.get(j+1));
+                      list.set(j+1, temp);
+                  }
+              }
+          }
+      }
+      return list;
   }
 
   public String[] insertion(String[] list, boolean ascending) {
