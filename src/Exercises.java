fe9e43b9e07fc91b3f07dfb440e69bcd112186d1
terzoc <doctort123@gmail.com>
2020-03-22 19:55:44 -0400

    start 8

--- src/Exercises.java
+++ src/Exercises.java
@@ -132,7 +132,27 @@ public class Exercises {
   }
 
   public String[] insertion(String[] list, boolean ascending) {
-    return null;
+    if (list == null || list.size() == 0) {
+          return null;
+      }
+
+      if (ascending) {
+          for (int i = 1; i < list.size(); i++) {
+              int key = list.get(i);
+              for (int j = i - 1; j >= 0; j--) {
+                  if (key < list.get(j)) {
+                      list.set(j + 1,list.get(j));
+                      if (j == 0) {
+                          list.set(0, key);
+                      }
+                  } else {
+                      list.set(j + 1, key);
+                      break;
+                  }
+              }
+          }
+      }
+      return list;
   }
 
   public int[] selection(int[] list, boolean ascending) {
