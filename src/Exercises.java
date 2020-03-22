f7e28c49d97b6e6167c5f72d5f38765233f2c041
terzoc <doctort123@gmail.com>
2020-03-22 19:41:33 -0400

    Exericise 6 done

--- src/Exercises.java
+++ src/Exercises.java
@@ -95,11 +95,11 @@ public class Exercises {
 
       if (ascending) {
           for (int i = 0; i < list.size() -1; i++) {
-              for (int j = 0; j < list.size() -1 -i; j++) {
-                  if (list.get(j) > list.get(j + 1) {
-                    String temp = list.get(j);
-                    list.set(j, list.get(j+1));
-                    list.set(j+1, temp);
+              for (int j = 0; j < list.size()-1-i; j++) {
+                  if (list.get(j).compareTo(list.get(j+1)) > 0) {
+                      String temp = list.get(j);
+                      list.set(j, list.get(j+1));
+                      list.set(j+1, temp);
                   }
               }
           }
